d14798a97b651c2ff7a79ee3ee4f668246c5e1e3
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-01-19 19:35:01 -0700

    tweeked my code/tooltip

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,8 @@ var margin = {
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+
+
 var svg = d3
   .select("#scatter")
   .append("svg")
@@ -35,11 +37,11 @@ d3.csv("./assets/data/data.csv").then(function(healthData){
 
 // create scales
   var xScale = d3.scaleLinear()
-  .domain([0, d3.max(healthData, d => d.poverty)])
+  .domain([d3.min(healthData, d => d. poverty)*0.9, d3.max(healthData, d => d.poverty)*1.1])
   .range([0, width]);
 
   var yScale = d3.scaleLinear()
-  .domain([0, d3.max(healthData, d => d.healthcare)])
+  .domain([d3.min(healthData, d => d. healthcare)*0.8, d3.max(healthData, d => d.healthcare)*0.85])
   .range([height, 0]);
 
 //Create axis functions
@@ -61,8 +63,9 @@ d3.csv("./assets/data/data.csv").then(function(healthData){
     .append("circle")
     .attr("cx", d => xScale(d.poverty))
     .attr("cy", d => yScale(d.healthcare))
-    .attr("r", "15")
+    .attr("r", "16")
     .attr("fill", "lightblue")
+    .style("text-align", "center")
     .attr("opacity", ".5");
 
 // Create circle labels
@@ -70,24 +73,25 @@ d3.csv("./assets/data/data.csv").then(function(healthData){
     .data(healthData)
     .enter()
     .append("text")
-    .attr("x", d => xScale(d.poverty) - 5)
-    .attr("y", d => yScale(d.healthcare) + 3)
-    .attr("fill", "white")
-    .attr("font-size", "8")
+    .style("text-align", "center")
+    .attr("x", d => xScale(d.poverty) -5)
+    .attr("y", d => yScale(d.healthcare) +5)
+    .style("fill", "black")
+    .attr("font-size", "11")
     .text(d => d.abbr);
 
 //Initialize tool tip
   var toolTip = d3.tip()
-    .attr("class", "tooltip")
+    .attr("class", "tooltip d3-tip")
     .offset([80, -60])
      .html(function(d) {
-       return (`${d.state}<br>Hair length: ${d.poverty}<br>Hits: ${d.healthcare}`);
+       return (`${d.state}<br>poverty: ${d.poverty}%<br>healthcare: ${d.healthcare}%`);
      });
 // Create tooltip in the chart
   chartGroup.call(toolTip);
 
 // Create event listeners to display and hide the tooltip
-  circlesGroup.on("click", function(data) {
+  circlesGroup.on("mouseover", function(data) {
     toolTip.show(data, this);
    })
   // onmouseout event
@@ -102,12 +106,33 @@ d3.csv("./assets/data/data.csv").then(function(healthData){
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .attr("class", "axisText")
+    .classed("active", true)
     .text("Lacks Healthcare (%)");
 
   chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
     .attr("class", "axisText")
+    .classed("active", true)
     .text("In Poverty (%)");
   }).catch(function(error) {
     console.log(error);
+
+  // changes classes to change bold text
+      if (chosenXAxis === "poverty") {
+        albumsLabel
+          .classed("active", true)
+           .classed("inactive", false);
+        hairLengthLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        }
+      else {
+        albumsLabel
+          .classed("active", false)
+          .classed("inactive", true);
+        hairLengthLabel
+           .classed("active", true)
+           .classed("inactive", false);
+        }
+
   });
